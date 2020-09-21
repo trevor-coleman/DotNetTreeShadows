51c8b59944d44efdeda5976d6285e7cd301a1974
Trevor Coleman <trevor@trevorcoleman.design>
2020-09-21 12:59:50 -0400

    disable unneeded warnings and remove underscore from field name.

--- dotnet-tree-shadows/Pages/Error.cshtml.cs
+++ dotnet-tree-shadows/Pages/Error.cshtml.cs
@@ -2,13 +2,15 @@ using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+#pragma warning disable 8618
 
 namespace dotnet_tree_shadows.Pages {
     [ResponseCache( Duration = 0, Location = ResponseCacheLocation.None, NoStore = true )]
     public class ErrorModel : PageModel {
+        // ReSharper disable once NotAccessedField.Local
         private readonly ILogger<ErrorModel> logger;
 
-        public ErrorModel (ILogger<ErrorModel> _logger) { logger = _logger; }
+        public ErrorModel (ILogger<ErrorModel> logger) { this.logger = logger; }
         public string RequestId { get; set; }
 
         public bool ShowRequestId => !string.IsNullOrEmpty( RequestId );
