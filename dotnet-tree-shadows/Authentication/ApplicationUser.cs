dcab77bfd3a04948ff2b45122ab8e59b7e82a510
Trevor Coleman <trevor@trevorcoleman.design>
2020-09-21 12:52:37 -0400

    Add UserId property.

--- dotnet-tree-shadows/Authentication/ApplicationUser.cs
+++ dotnet-tree-shadows/Authentication/ApplicationUser.cs
@@ -2,5 +2,8 @@ using AspNetCore.Identity.Mongo.Model;
 
 namespace dotnet_tree_shadows.Authentication {
     public class ApplicationUser:MongoUser {
+        public string UserId {
+            get => Id.ToString();
+        }
     }
 }
