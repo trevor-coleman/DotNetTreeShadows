8e717beb4667f97ff3dc84523f9f397a12d88505
Trevor Coleman <trevor@trevorcoleman.design>
2020-10-01 13:58:09 -0400

    Remove Old Files

--- DotNetTreeShadows/Models/SessionModels/TileDTO.cs
+++
@@ -1,8 +0,0 @@
-namespace dotnet_tree_shadows.Models.SessionModels {
-    public struct OldTileDto {
-        public HexCoordinates HexCoordinates { get; set; }
-        public PieceType? PieceType { get; set; }
-        public TreeType? TreeType { get; set; }
-        public int ShadowHeight { get; set; }
-    }
-}
